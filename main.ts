62249885aae9ff84909c95dcb440e42003d9c596
albert926 <103927053+albert926@users.noreply.github.com>
2025-11-05 14:27:39 +0800

    Just added some stuff, including sprites

--- main.ts
+++ main.ts
@@ -1,2 +1,21 @@
+let bird = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+info.setScore(0)
 scene.setBackgroundImage(assets.image`background`)
 music.play(music.createSong(assets.song`funMusic`), music.PlaybackMode.LoopingInBackground)
