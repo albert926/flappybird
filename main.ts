44a35e0480a33a370d04d78a98ad89cc65e8aa72
albert926 <103927053+albert926@users.noreply.github.com>
2025-11-10 14:09:35 +0800

    Added better control system and better visuals

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    bird.setVelocity(0, -100)
+    bird.setVelocity(0, -120)
+    pause(100)
+    bird.setVelocity(0, 70)
+    pauseUntil(() => !(controller.A.isPressed()))
 })
 function checkEnemyMove () {
     if (enemyMove == 1) {
@@ -19,9 +22,69 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
     music.play(music.createSong(assets.song`funMusic0`), music.PlaybackMode.LoopingInBackground)
     game.gameOver(false)
 })
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    bird.setVelocity(0, 50)
-})
+function updateAnimation () {
+    if (bird.y < lastY) {
+        lastY = bird.y
+        bird.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . b b b b b 5 5 5 5 5 5 5 b . . 
+            . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+            . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+            . . b d 5 5 b 1 f f 5 4 4 c . . 
+            b b d b 5 5 5 d f b 4 4 4 4 4 b 
+            b d d c d 5 5 b 5 4 4 4 4 4 b . 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `)
+        pause(500)
+    } else {
+        bird.setImage(img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b c . . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            b d d d b b d 5 5 5 4 4 4 4 4 b 
+            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+            c d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c b d d c c b 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `)
+        pause(200)
+        bird.setImage(img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 d 4 c . . 
+            . . . . b 5 5 1 f f d d 4 4 4 b 
+            . . . . b 5 5 d f b 4 4 4 4 b . 
+            . . . b d 5 5 5 5 4 4 4 4 b . . 
+            . b b d d d 5 5 5 5 5 5 5 b . . 
+            b d d d b b b 5 5 5 5 5 5 5 b . 
+            c d d b 5 5 d c 5 5 5 5 5 5 b . 
+            c b b d 5 d c d 5 5 5 5 5 5 b . 
+            c b 5 5 b c d d 5 5 5 5 5 5 b . 
+            b b c c c d d d 5 5 5 5 5 d b . 
+            . . . . c c d d d 5 5 5 b b . . 
+            . . . . . . c c c c c b b . . . 
+            `)
+        pause(500)
+    }
+}
 function hardLevel () {
     setup(2700, 2900, 230, 1)
     followSpeed = 15
@@ -52,6 +115,7 @@ let enemyMovementSpeed = 0
 let enemyGenTime = 0
 let candyGen = 0
 let candy: Sprite = null
+let lastY = 0
 let myEnemy: Sprite = null
 let enemyMove = 0
 let bird: Sprite = null
@@ -78,7 +142,6 @@ bird = sprites.create(img`
     . . . . c c d d d 5 5 5 b b . . 
     . . . . . . c c c c c b b . . . 
     `, SpriteKind.Player)
-bird.x = 30
 animation.runImageAnimation(
 bird,
 [img`
@@ -184,17 +247,124 @@ bird,
     . . . c c c c c c c c b b . . . 
     . . . . . . . . . . . . . . . . 
     `],
-200,
+500,
 true
 )
+bird.x = 30
 info.setScore(0)
 scene.setBackgroundImage(assets.image`background`)
 scroller.scrollBackgroundWithSpeed(-100, 0)
-bird.setVelocity(0, 50)
+bird.setVelocity(0, 70)
 bird.setStayInScreen(true)
 forever(function () {
     myEnemy = sprites.createProjectileFromSide(assets.image`pillar`, enemyMovementSpeed * -1, 0)
     myEnemy.y = randint(15, 115)
+    animation.runImageAnimation(
+    myEnemy,
+    [img`
+        ..............ffffff....
+        .............f2feeeeff..
+        ............f222feeeeff.
+        .......cc...feeeeffeeef.
+        .......cdc.fe2222eeffff.
+        .......cddcf2effff222ef.
+        ........cddcffeeefffffff
+        .........cddce44fbe44eff
+        ..........cdceddf14d4eef
+        ..........cccdeddd4eeef.
+        ...........edd4e44eeff..
+        ............ee442222f...
+        .............f2e2222f...
+        .............f554444f...
+        ..............ffffff....
+        ................fff.....
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `,img`
+        ........................
+        ..............fff.......
+        .............f2fffff....
+        ...........ff22eeeeeff..
+        ..........ff222eeeeeeff.
+        ..........feeeefffeeeef.
+        .........fe2222eeefffff.
+        .........f2efffff222efff
+        ..cc.....fffeeefffffffff
+        ..cdcc...fee44fbbe44efef
+        ..ccddcc..feddfbb4d4eef.
+        ....cdddceefddddd4eeef..
+        .....ccdcddee2222222f...
+        ......cccdd44e544444f...
+        .........eeeeffffffff...
+        .............ff...fff...
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `,img`
+        ...............ff.......
+        .............ff2ffff....
+        ............ff2feeeeff..
+        ...........ff22feeeeeff.
+        ...........feeeeffeeeef.
+        ..........fe2222eefffff.
+        ..........f2effff222efff
+        ..........fffeeeffffffff
+        ..........fee44fbe44efef
+        ...........feddfb4d4eef.
+        ..........c.eeddd4eeef..
+        ....ccccccceddee2222f...
+        .....dddddcedd44e444f...
+        ......ccccc.eeeefffff...
+        ..........c...ffffffff..
+        ...............ff..fff..
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `,img`
+        ..............ffffff....
+        .............f2feeeeff..
+        ............f222feeeeff.
+        ............feeeeffeeef.
+        ...........fe2222eeffff.
+        ...........f2effff222ef.
+        ...........fffeeefffffff
+        ...........fee44fbe44eff
+        ............feddf14d4eef
+        .............fdddd4eeef.
+        .............fe444eddf..
+        .............ccc22eddf..
+        .............cdc22fee...
+        ............cddc4444f...
+        ...........cddcfffff....
+        ..........cddc..fff.....
+        ..........cdc...........
+        ..........cc............
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `],
+    500,
+    true
+    )
     checkEnemyMove()
     pause(enemyGenTime)
     sprites.destroy(myEnemy, effects.spray, 500)
